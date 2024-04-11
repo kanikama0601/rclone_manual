24b89fe9041501138292506e124d37bbfc026ec9
kanikama0601 <kanikama0601school@gmail.com>
2024-04-12 01:45:09 +0900

    commit

--- resouces/rclone_setup_tool_js2.sh
+++ resouces/rclone_setup_tool_js2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while :; do
-	rclone sync ~/js2 google_drive:js2 --update --delete-before --progress
-	rclone sync google_drive:js2 ~/js2 --update --delete-before --progress
+	rclone sync ~/js2 google_drive:js2 --update --delete-after --progress
+	rclone sync google_drive:js2 ~/js2 --update --delete-after --progress
 	chmod -R 777 ~/js2
 	sleep 180
 done
